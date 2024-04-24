da483a8df57a8a1adad99f5fb51825690a1e7241
TeeZeeeh <tajrina.promela@gmail.com>
2024-04-24 22:05:59 +0600

    updated UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,11 @@ import streamlit as st
 
 st.set_page_config(page_title="Echocardiogram Analysis", page_icon="heart", layout="wide")
 
-st.header("Echocardiogram Analysis")
+st.header("Deep Vision Prognosis of Cardiac Arrest using Echocardiograms")
 
-uploaded_files = st.file_uploader(label = "Upload your echocardiogram", type = ["avi"], accept_multiple_files = False)
+uploaded_files = st.file_uploader(label="Upload your echocardiogram", type=["avi"], accept_multiple_files=False)
+
+display_file = st.video(uploaded_files, format="video/avi", start_time=0, subtitles=None, end_time=None, loop=False)
 
 
 
