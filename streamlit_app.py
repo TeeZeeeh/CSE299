eeca9ac9b68d67b478226b5e35406ed9caf496ce
TeeZeeeh <tajrina.promela@gmail.com>
2024-04-24 21:48:37 +0600

    Updated UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="Echocardiogram Analysis", page_icon="heart", layout="wide")
+
+st.header("Echocardiogram Analysis")
+
+uploaded_files = st.file_uploader(label = "Upload your echocardiogram", type = ["avi"], accept_multiple_files = False)
+
+
 
