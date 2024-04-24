1f5b092835708cbacf8f7ed06ba8ded0c6d2290c
TeeZeeeh <tajrina.promela@gmail.com>
2024-04-25 02:07:49 +0600

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,8 @@ with st.status("Calculating the Ejection Fraction..."):
     st.write("Semantically Segmenting the Left-Ventricle...")
     time.sleep(2)
     
-    col1, col2, col3 = st.columns(3)
-    with col2:
+    col1, col2, col3, col4, col5 = st.columns(3)
+    with col3:
         st.video("c6faad01-66db-4275-b246-892dec7a4067.mp4")
 
     st.write("Predicting the Ejection Fraction...")
