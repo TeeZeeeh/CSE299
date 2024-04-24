226c8f10ce568038d7f459b78576e5b20a4ac8d6
TeeZeeeh <tajrina.promela@gmail.com>
2024-04-25 00:00:33 +0600

    updated UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,31 @@
 import streamlit as st
+import time
+import pandas as pd
 
-st.set_page_config(page_title="Echocardiogram Analysis", page_icon="heart", layout="wide")
+st.set_page_config(page_title="Echocardiogram Analysis", page_icon="❤️", layout="wide")
 
 st.header("Deep Vision Prognosis of Cardiac Arrest using Echocardiograms")
 
 uploaded_files = st.file_uploader(label="Upload your echocardiogram", type=["avi"], accept_multiple_files=False)
 
-display_file = st.video(uploaded_files, format="video/avi", start_time=0, subtitles=None, end_time=None, loop=False)
+with st.status("Calculating the Ejection Fraction..."):
+    st.write("Downloading data...")
+    time.sleep(2)
+    st.write("Loading model...")
+    time.sleep(2)
+    st.write("Semantically Segmenting the Left-Ventricle...")
+    time.sleep(2)
+    st.write("Predicting the Ejection Fraction...")
+    time.sleep(1)
+    st.write("Assessing Cardiomyopathy...")
+    time.sleep(2)
 
+st.success('Done!', icon="✅")
 
+filelist = pd.read_csv("FileList.csv")
+st.table(filelist)
+
+st.markdone("The Ejection Fraction is: ")
+
+st.button('Rerun')
 
