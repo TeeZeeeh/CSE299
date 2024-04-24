9bd64d19816091e911bb04be7bf5d5e2e1c1c162
TeeZeeeh <tajrina.promela@gmail.com>
2024-04-25 03:16:23 +0600

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,24 +8,25 @@ st.header("Deep Vision Prognosis of Cardiac Arrest using Echocardiograms")
 
 uploaded_files = st.file_uploader(label="Upload your echocardiogram", type=["avi"], accept_multiple_files=False)
 
-with st.status("Calculating the Ejection Fraction..."):
-    st.write("Downloading data...")
-    time.sleep(2)
-    st.write("Loading model...")
-    time.sleep(2)
-    st.write("Semantically Segmenting the Left-Ventricle...")
-    time.sleep(2)
+if uploaded_files is not None:
+    with st.status("Calculating the Ejection Fraction...", expanded=True):
+        st.write("Downloading data...")
+        time.sleep(2)
+        st.write("Loading model...")
+        time.sleep(2)
+        st.write("Semantically Segmenting the Left-Ventricle...")
+        time.sleep(2)
     
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col3:
-        st.video("c6faad01-66db-4275-b246-892dec7a4067.mp4")
+        col1, col2, col3, col4, col5 = st.columns(5)
+        with col3:
+            st.video("c6faad01-66db-4275-b246-892dec7a4067.mp4")
 
-    st.write("Predicting the Ejection Fraction...")
-    time.sleep(1)
-    st.write("Assessing Cardiomyopathy...")
-    time.sleep(2)
+        st.write("Predicting the Ejection Fraction...")
+        time.sleep(1)
+        st.write("Assessing Cardiomyopathy...")
+        time.sleep(2)
 
-st.success('Done!', icon="✅")
+    st.success('Done!', icon="✅")
 
 filelist = pd.read_csv("FileList.csv")
 st.table(filelist)
